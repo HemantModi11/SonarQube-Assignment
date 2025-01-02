573226d915d8c2f789f1b21875256c1e324b0cd9
Hemant Modi <90129538+HemantModi11@users.noreply.github.com>
2025-01-02 14:38:03 +0530

    Update userModel.js

--- model/userModel.js
+++ model/userModel.js
@@ -3,7 +3,8 @@ const mongoose=require('mongoose');
 const userSchema=new mongoose.Schema({
     name:String,
     email:String,
-    password:String
+    password:String,
+    newPassword:String
 })
 
 const UserModel=mongoose.model("user",userSchema);
